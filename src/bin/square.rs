c6e6c4811d19dc06a75ca4eb6bdf1d905e2f52f3
Diogo Lundberg <dclundberg@gmail.com>
2022-07-24 21:32:06 +0200

    Resize square

--- src/bin/square.rs
+++ src/bin/square.rs
@@ -7,15 +7,17 @@ async fn main() {
     loop {
         const TEXT: &str = "A bloody square";
 
+        let size = screen_height() / 2.;
+
         let square = Rect {
-            x: screen_width() / 2. - 100.,
-            y: screen_height() / 2. - 100.,
-            w: 200.,
-            h: 200.,
+            x: screen_width() / 2. - size / 2.,
+            y: screen_height() / 2. - size / 2.,
+            w: size,
+            h: size,
         };
 
         draw_rectangle(square.x, square.y, square.w, square.h, RED);
-        draw_text(TEXT, square.x, square.y, 30., WHITE);
+        draw_text(TEXT, square.x, square.y, size/7., WHITE);
         next_frame().await;
     }
 }
